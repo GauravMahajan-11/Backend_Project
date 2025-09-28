b84ab3854a57f543afbc3a46da94a4d88885ab31
Gaurav Mahajan <gauravmahajan4321@gmail.com>
2025-09-28 19:05:04 +0530

    added cloudinary in utils and multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+
+const uploadOnCloudinary = async (localFilePAth) => {
+    try{
+          if(!localFilePAth) return null
+          // upload the file on cloudinary
+
+          const response = await cloudinary.uploader.upload(localFilePAth, {
+            resource_type : "auto" 
+          })
+
+          //file uploaded successfully
+          console.log(" FILE UPLOADED ON CLOUDINARY  ", response.url)
+          return response;
+    }
+    catch(err){
+        fs.unlinkSync(localFilePAth) //remove the temporry file from local storage as 
+        // upload operation failed
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
