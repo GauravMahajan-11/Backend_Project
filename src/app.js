6214c766491e9003d928ca8a449be253ddfcb146
Gaurav Mahajan <gauravmahajan4321@gmail.com>
2025-09-29 15:20:36 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extented:true,limit:"16kb"}))
 app.use(cookieParser())
 
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter) //http://localhost:5000/users
+
+
+
 
 export {app}
